cf96beab0eb3fc06a5481b4ab56851dc236979bb
lanitochka <70783335+lanitochka@users.noreply.github.com>
2021-11-05 15:23:03 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
 
+
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextareaInput, 500));
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    formData.email = form.email.value;
+    formData.message = form.message.value;
+
+    console.log(formData);
+    
+    localStorage.removeItem(STORAGE_KEY);
+
+    formData.email = '';
+    formData.message = '';
+    e.currentTarget.reset();
+}
+
+
+function onTextareaInput(e) {   
+
+    formData[e.target.name] = e.target.value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+
+function populateTextarea() {
+    const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));   
+
+    if (savedForm) {
+        form.email.value = savedForm.email;
+        form.message.value = savedForm.message;
+        
+        formData.email = savedForm.email;
+        formData.message = savedForm.message;
+    }
+
+}
+
+populateTextarea();
