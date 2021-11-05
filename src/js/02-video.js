7d64b629d3a47ef4fc699038cde9dc4e7b50f221
lanitochka <70783335+lanitochka@users.noreply.github.com>
2021-11-05 14:40:49 +0100

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+var iframe = document.querySelector('iframe');
+var player = new Player(iframe);
+
+player.on('timeupdate', throttle(function (data) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}, 1000));
+
+const savedTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
+player.setCurrentTime(savedTime.seconds);
